3cd729fab82c031c942190dd43b1b46d41102a46
KrisBoris <k.store@interia.eu>
2024-08-04 18:11:08 +0200

    Example 9

--- App9_NavigationDrawer/app/src/main/java/com/example/app9_navigationdrawer/MainActivity.kt
+++ App9_NavigationDrawer/app/src/main/java/com/example/app9_navigationdrawer/MainActivity.kt
@@ -1,20 +1,79 @@
 package com.example.app9_navigationdrawer
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout : DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        Log.i("TEST_TAG", "After drawerLayout")
+
+        val toolBar = findViewById<Toolbar>(R.id.toolBar)
+        setSupportActionBar(toolBar)
+        Log.i("TEST_TAG", "After toolBar")
+
+        val navigationView = findViewById<NavigationView>(R.id.navView)
+        navigationView.setNavigationItemSelectedListener(this)
+        Log.i("TEST_TAG", "After navigationView")
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        Log.i("TEST_TAG", "After toggle")
+
+        if(savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+            navigationView.setCheckedItem(R.id.navHome)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            when(item.itemId) {
+                R.id.navHome -> replaceFragment(HomeFragment())
+                R.id.navAbout -> replaceFragment(AboutFragment())
+                R.id.navSettings -> replaceFragment(SettingsFragment())
+                R.id.navShare -> replaceFragment(ShareFragment())
+                R.id.navLogout -> Toast.makeText(this, "Logout", Toast.LENGTH_LONG).show()
+            }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START)
+        else {
+            super.onBackPressed()
         }
     }
 }
