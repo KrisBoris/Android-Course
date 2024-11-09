8e95952129f844a98fbf99aeb24d40764ce6740d
KrisBoris <k.store@interia.eu>
2024-11-09 21:49:25 +0100

    News App - Retrofit

---
+++ App42_NewsApp/app/src/main/java/com/example/app42_newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package com.example.app42_newsapp.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ App42_NewsApp/app/src/main/java/com/example/app42_newsapp/util/Constans.kt
@@ -0,0 +1,10 @@
+package com.example.app42_newsapp.util
+
+class Constans {
+    companion object {
+        const val API_KEY = "9f57c7e3de8e450b8638bc129c0a2798"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
